429b262189d4702436cc1de1dc071d6dda0d23e5
MG_Cree <75585687+MGCree@users.noreply.github.com>
2022-11-04 10:44:13 +0000

    greetings working
    Signed-off-by: MG_Cree  <75585687+MGCree@users.noreply.github.com>

--- greetings.py
+++ greetings.py
@@ -3,4 +3,6 @@ import random
 greetings = ["Hi","Hello","Greetings sir","Greetings young Master"]
 
 def greet():
-    print(random(greetings))
+    greeting = random.choice(greetings)
+
+    print(greeting)
