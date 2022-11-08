1e5d42fde3e93bf3c37bcedfb9110a1133cf7f78
MG_Cree <jakub2910@hotmail.com>
2022-11-08 21:52:02 +0000

    webManager: fixed wording

--- webManager.py
+++ webManager.py
@@ -1,6 +1,6 @@
 import webbrowser as web
 
 def openWebPage():
-    url = input("What would you like to surch for:  ")
+    url = input("What would you like to search for:  ")
 
     web.open(url, new=2)
