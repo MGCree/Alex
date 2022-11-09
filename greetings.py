1f350eeb2562365b5ebf22932f67b20163e6fcaa
MG_Cree <jakub2910@hotmail.com>
2022-11-09 12:34:46 +0000

    tts

--- greetings.py
+++ greetings.py
@@ -1,12 +1,18 @@
 import random
 import pyttsx3
 
-engine = pyttsx3.init()
+
 
 greetings = ["Hi","Hello","Greetings sir","Greetings young Master"]
 
 def greet():
     
+    engine = pyttsx3.init()
+    engine.setProperty('volume',1.0)
+    engine.setProperty('rate', 150)
+    engine.setProperty('voice', voices[0].id)
+
     greeting = random.choice(greetings)
 
     engine.say(greeting)
+    engine.runAndWait()
