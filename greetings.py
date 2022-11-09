c6bcafd054334d9a25685d91f572de5f2a03cf93
MG_Cree <jakub2910@hotmail.com>
2022-11-09 18:44:22 +0000

    un py

--- greetings.py
+++ greetings.py
@@ -1,17 +1,9 @@
 import random
-import pyttsx3
-
-
 
 greetings = ["Hi","Hello","Greetings sir","Greetings young Master"]
 
 def greet():
-    
-    engine = pyttsx3.init()
-    engine.setProperty('volume',1.0)
-    engine.setProperty('rate', 150)
-
+   
     greeting = random.choice(greetings)
 
-    engine.say(greeting)
-    engine.runAndWait()
+    print(greeting)
